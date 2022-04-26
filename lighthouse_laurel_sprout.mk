7671b1529e8c25cfb76c9d210f60548a480b9ecb
Manoj Krish <93692786+manojkrishkumar@users.noreply.github.com>
2022-04-26 10:25:13 +0530

    laurel_sprout: lighthouse bringup

--- lighthouse_laurel_sprout.mk
+++ lighthouse_laurel_sprout.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 Spark Rom Project
+# Copyright (C) 2022 Lighthouse Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common Spark stuff
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Lighthouse stuff
+$(call inherit-product, vendor/lighthouse/config/common.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
@@ -29,7 +29,7 @@ PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := spark_laurel_sprout
+PRODUCT_NAME := lighthouse_laurel_sprout
 PRODUCT_MODEL := Mi A3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -51,11 +51,11 @@ EXTRA_UDFPS_ICONS := true
 EXTRA_UDFPS_ANIMATIONS := true
 
 # Props
-SPARK_BUILD_TYPE := UNOFFICIAL
+LIGHTHOUSE_BUILD_TYPE := UNOFFICIAL
 WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED=true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_GAPPS_ARCH := arm64
 TARGET_HAS_FOD := true
 TARGET_USES_BLUR := true
