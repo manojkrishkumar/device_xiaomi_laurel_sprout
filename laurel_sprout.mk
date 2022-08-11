d9fb95d9eab56967c348ad78bd72ce5cfb9a773f
Magicxavi <rolfpeterkesche@gmail.com>
2022-08-11 10:34:19 +0530

    laurel_sprout: Delete Gcam and build additional camera blobs

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -116,11 +116,16 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hidl.memory.block@1.0.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    GCamGo
-
+    Snap
+    
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
