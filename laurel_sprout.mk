3ecff23ca8634617884c6b214c14477c8e21a4f0
Manoj Krish <93692786+manojkrishkumar@users.noreply.github.com>
2022-08-11 14:41:40 +0530

    laurel_sprout: Add support for Google Dialer Call recording

--- laurel_sprout.mk
+++ laurel_sprout.mk
@@ -108,6 +108,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Call recording
+PRODUCT_PACKAGES += \
+    com.google.android.apps.dialer.call_recording_audio.features.xml
+    
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
