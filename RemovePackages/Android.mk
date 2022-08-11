9a64ef3809a363680e749a97416047f161b37e70
Manoj Krish <93692786+manojkrishkumar@users.noreply.github.com>
2022-08-11 10:16:51 +0530

    laurel_sprout: Remove most of the packages
    Laurel_Sprout partition limitations

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,11 +4,51 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt Camera2 CarrierSetup
-LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
-LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+        Chrome-Stub \
+        Chrome \
+        Drive \
+        Maps \
+        Photos \
+        PrebuiltGmail \
+        Videos \
+        YouTube \
+        FilesPrebuilt \
+        AndroidAutoStubPrebuilt \
+        AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	CalendarGooglePrebuilt \
+	Camera2 \
+	CarrierSetup \
+	ConnMO \
+	DCMO \
+	DMService \
+	DevicePolicyPrebuilt \
+	DiagnosticsToolPrebuilt \
+	SafetyHubPrebuilt \
+	HelpRtcPrebuilt \
+	GCS \
+	MyVerizonServices \
+	NgaResources \
+	OBDM_Permissions \
+	OemDmTrigger \
+	PixelLiveWallpaperPrebuilt \
+	PixelWallpapers2021 \
+	Music \
+	RecorderPrebuilt \
+	ScribePrebuilt \
+	Showcase \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM \
+	Tycho \
+	USCCDM \
+	VZWAPNLib \
+        SCONE \
+        YouTubeMusicPrebuilt \
+        arcore \
+	VzwOmaTriger
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
